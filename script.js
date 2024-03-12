c21ac8de53927c2648172ee49395a3f69a2ae499
cpreza24 <cpreza93@yahoo.com>
2024-03-11 21:07:27 -0700

    appends a new task to the parent div

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let form = document.querySelector('.taskForm');
 let inputBox = document.querySelector('.submitInput');
+let tasksContainer = document.querySelector('.tasksContainer');
 let inputValue;
 let tasks = [];
 
@@ -10,14 +11,18 @@ inputBox.addEventListener('input', e => {
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    addNewTask(inputValue);
     tasks.push(inputValue);
-    console.log(tasks);
+    newTask.appendChild(inputValue[0]);
     inputBox.value = '';
+    console.log(tasks);
 })
 
-
-
-
-
+function addNewTask(toDo) {
+    let newTask = document.createElement('p');
+    let taskValue = document.createTextNode(toDo);
+    newTask.appendChild(taskValue);
+    tasksContainer.appendChild(newTask);
+}
 
 
