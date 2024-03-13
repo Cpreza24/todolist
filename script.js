801dd985eda5b51ce3dd90d1bce89f4a79e5e9f2
cpreza24 <cpreza93@yahoo.com>
2024-03-12 21:17:28 -0700

    adds new todo to an arrray

--- script.js
+++ script.js
@@ -6,18 +6,17 @@ let tasks = [];
 
 inputBox.addEventListener('input', e => {
     inputValue = e.target.value;
-    console.log(inputValue);
 })
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    inputBox.value = '';
     addNewTask(inputValue);
     tasks.push(inputValue);
-    newTask.appendChild(inputValue[0]);
-    inputBox.value = '';
-    console.log(tasks);
+    console.log(tasks, tasks.length);
 })
 
+// Handles the creation of a new task when
 function addNewTask(toDo) {
     let newTask = document.createElement('p');
     let taskValue = document.createTextNode(toDo);
@@ -26,3 +25,4 @@ function addNewTask(toDo) {
 }
 
 
+// I think i have to loop through the array now because im getting undefined. 
